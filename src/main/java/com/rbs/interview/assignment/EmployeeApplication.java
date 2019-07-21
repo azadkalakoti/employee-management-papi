fe0523b95433947eb1db0f371cdb8f70cf8db2f0
Azad Singh <Azad Singh@LAPTOP-305JUIRP>
2019-07-21 15:49:39 +0530

    first commit

--- src/main/java/com/rbs/interview/assignment/EmployeeApplication.java
+++ src/main/java/com/rbs/interview/assignment/EmployeeApplication.java
@@ -2,13 +2,11 @@ package com.rbs.interview.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = "com.rbs.interview.assignment")
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
